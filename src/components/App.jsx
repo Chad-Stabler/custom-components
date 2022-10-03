5d9576cedf27f25d733cd68a3b99a8919a1c0608
Chad Stabler <stablerpsn@gmail.com>
2022-10-03 12:48:40 -0500

    search form wired to app, dealing with errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,18 @@ import HomePage from './HomePage/HomePage';
 import Layout from './Page/Layout';
 import Planes from './Planes/Planes';
 import Trains from './Trains/Trains';
+import Search from './Search/Search.jsx';
 import Automobiles from './Automobiles/Automobiles';
 import Koenigsegg from './Automobiles/Koenigsegg';
 import Bugatti from './Automobiles/Bugatti';
 import Hennessy from './Automobiles/Hennessy';
 
-export default function app() {
+export default function App() {
   return (<Router> 
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="search" element={<Search />} />
         <Route path="planes" element={<Planes />} />
         <Route path="trains" element={<Trains />} />
         <Route path="automobiles" element={<Automobiles />} >
--- src/components/Page/Navigation.jsx
+++ src/components/Page/Navigation.jsx
@@ -4,6 +4,7 @@ import styles from './Navigation.css';
 export default function Navigation() {
   return (<nav className={styles.Navigation}>
     <NavLink to="">Home</NavLink>
+    <NavLink to="search">Search Beanie Babaies</NavLink>
     <NavLink to="planes">Planes</NavLink>
     <NavLink to="trains">Trains</NavLink>
     <NavLink to="automobiles">Automobiles</NavLink>
